3b2a2b2f9e113ad125d1aaf8cfbbee04c9365f37
Mudassirkhan2 <106579572+Mudassirkhan2@users.noreply.github.com>
2022-12-08 22:43:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+// Your challenge is to build out this advice generator app using the [Advice Slip API](https://api.adviceslip.com) and get it looking as close to the design as possible.
+const advice=document.querySelector(".advice")
+const id =document.getElementById("id")
+let p = fetch("https://api.adviceslip.com/advice")
+p.then((response)=>{
+
+    return response.json()
+}).then((data)=>{
+    let data1 = data
+    console.log(data1.slip.id)
+    console.log(data1.slip.advice)
+    advice.innerHTML=data1.slip.advice
+    id.innerHTML=`"${data1.slip.id} "`
+})
