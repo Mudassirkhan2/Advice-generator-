8f9dcc666a4d2e917aabed952a63cac0709dee60
Mudassirkhan2 <106579572+Mudassirkhan2@users.noreply.github.com>
2022-12-12 09:47:07 +0530

    updated script added button functionality

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 // Your challenge is to build out this advice generator app using the [Advice Slip API](https://api.adviceslip.com) and get it looking as close to the design as possible.
 const advice=document.querySelector(".advice")
-const id =document.getElementById("id")
+const id =document.getElementById("id");
+const btn =document.getElementById("btn");
+
 let p = fetch("https://api.adviceslip.com/advice")
 p.then((response)=>{
 
@@ -11,4 +13,8 @@ p.then((response)=>{
     console.log(data1.slip.advice)
     advice.innerHTML=data1.slip.advice
     id.innerHTML=`"${data1.slip.id} "`
+})
+btn.addEventListener("click",()=>{
+    location.reload();
+    console.log("btn clicked")
 })
